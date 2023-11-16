f90e9567ada14bbcf2ff46dff932fb8e77a178f7
Gabriel Mendonca <gabriel.mendonca@okn.com.br>
2023-11-15 23:03:28 -0300

    Update Makefile and kernel code

--- arch/x86/kernel/kernel.c
+++ arch/x86/kernel/kernel.c
@@ -1,14 +1,30 @@
-void print_string(const char* string) {
-    volatile char* video_memory = (volatile char*)0xB8000;
-    while (*string != 0) {
-        *video_memory++ = *string++;
-        *video_memory++ = 0x07;
-    }
+// A simple kernel to test the boot process
+
+// Function to write a character to screen at a given position
+void write_char(char c, unsigned char color, int x, int y) {
+    volatile char* video_memory = (volatile char*) 0xB8000;
+    int offset = (y * 80 + x) * 2; // 80 characters per line, 2 bytes per character
+    video_memory[offset] = c;
+    video_memory[offset + 1] = color;
 }
 
-void main() {
-    print_string("Hello, World!");
-    while (1) {
-
+// Kernel's main function
+void kernel_main() {
+    // Clear the screen by setting it all to spaces
+    for (int y = 0; y < 25; y++) {
+        for (int x = 0; x < 80; x++) {
+            write_char(' ', 0x07, x, y);
+        }
     }
+
+    // Write a simple message to the top-left of the screen
+    const char* message = "YrulOS v0.0.1";
+    int x = 0, y = 0;
+    while (message[x] != '\0') {
+        write_char(message[x], 0x07, x, y);
+        x++;
+    }
+
+    // Hang the kernel to prevent it from doing anything else
+    while (1) { }
 }
